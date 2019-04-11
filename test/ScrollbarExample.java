563e45f1255f39327afdf327bb9684b198b3c78b
Codechat <bhutadachaitanya@gmail.com>
2019-04-11 23:25:33 +0530

    Initial import

---
+++ test/ScrollbarExample.java
@@ -0,0 +1,19 @@
+//Create a Scrollbar and place it inside a frame
+import java.awt.*;
+class ScrollbarExample
+{
+  ScrollbarExample()
+  {
+	Frame f= new Frame("Scrollbar Example");
+	Scrollbar s=new Scrollbar();
+	s.setBounds(100,100, 30,200);
+	f.add(s);
+	f.setSize(400,400);
+	f.setLayout(null);
+	f.setVisible(true);
+  }
+  public static void main(String args[])
+  {
+    new ScrollbarExample();
+  }
+}
